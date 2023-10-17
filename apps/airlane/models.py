ea312b104ad1bc73a69658a51e51b77d51eb853c
zenix228 <zenixxx144@gmail.com>
2023-10-17 20:11:44 +0500

    ads

--- apps/airlane/models.py
+++ apps/airlane/models.py
@@ -4,7 +4,7 @@ from apps.plane.models import Plane
 
 class Airline(Model):
     name = CharField(max_length=511)
-    created_at = DateField()
+    created_at = DateField(blank=True, null=True)
     planes = ManyToOneRel(to='apps.plane.models.Plane', on_delete=CASCADE, related_name='planes', field='plane', field_name='plane')
 
     def __str__(self):
